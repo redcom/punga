cef4fa33bce38da30f1ea2a49ec52c32a9eb4320
Razvan Moraru <razvan@inclub.ro>
2017-02-10 21:38:10 +0100

    feat: cleanup header and add logo

---
+++ src/browser/app/Logo.js
@@ -0,0 +1,28 @@
+// @flow
+import type { State, User } from '../../common/types';
+import React from 'react';
+import { Box, Image } from '../../common/components';
+import { Link } from '../components';
+
+const getWebsiteLogo = () => require('./pinstery.png');
+
+const Logo = () => (
+  <Box
+    alignItems="center"
+    marginVertical={1}
+    paddingHorizontal={0.5}
+    borderWidth={1}
+    borderStyle="dotted"
+  >
+    <Link to="/">
+      <Image
+        src={getWebsiteLogo()}
+        alt="Go to Pinstery Home"
+        size={{ width: 250, height: 250 }}
+        marginVertical={2}
+      />
+    </Link>
+  </Box>
+);
+
+export default Logo;
